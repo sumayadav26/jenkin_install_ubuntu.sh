e449305bd2307d7bf7791244cef0b810db9887e9
sumayadav26 <116958108+sumayadav26@users.noreply.github.com>
2022-11-02 20:54:09 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+#!bin/bash/
+sudo apt-get update -y
+sudo apt-get install openjdk-8-jdk
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
