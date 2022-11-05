06812e04ddd9d073b1846838c8d2f1597951540a
sumayadav26 <116958108+sumayadav26@users.noreply.github.com>
2022-11-05 11:55:36 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!bin/bash/
 sudo apt-get update -y
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-11-jdk
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
